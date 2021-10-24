4035db0bfbb61d28440faa43863786f594d3ed23
Marcelo <marcelo.donadini@stp-argentina.com.ar>
2021-10-24 19:14:02 -0300

    Integrador

---
+++ Integrador/js/dom.js
@@ -0,0 +1,29 @@
+function descuento(){
+
+   var cantidad = document.getElementById("cantidad").value;
+   var descuento = document.getElementById("tipo").value;
+   document.getElementById("total").innerHTML ="Total a Pagar: $";
+
+   cantidad = cantidad*200;
+
+   if(descuento == "estudiante" ){
+       
+   	 var pago = (cantidad*80)/100;
+
+   } else if(descuento == "trainee"){
+   	
+       var pago = (cantidad*50)/100;
+   }else{
+   	
+       var pago = (cantidad*15)/100;
+   }
+   
+   document.getElementById("total").innerHTML += cantidad - pago;
+    return cantidad - pago;
+}
+
+function borrar(){
+document.getElementById("test").reset();
+document.getElementById("total").innerHTML ="Total a Pagar: $";
+
+}
